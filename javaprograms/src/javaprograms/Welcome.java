308ddf55c6e6122ff8e742fe0bd0d45c0e88c4f3
nraju85 <nraju85@gmail.com>
2022-07-14 20:56:12 +0530

    First commit

---
+++ javaprograms/src/javaprograms/Welcome.java
@@ -0,0 +1,11 @@
+package javaprograms;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Welcome to Eclipse");
+	}
+
+}
